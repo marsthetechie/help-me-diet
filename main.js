36cd03b4ad185e7963bf2a82aa4151abd626ed86
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-09-12 13:17:29 +0200

    more covers and title

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let cover1 = document.getElementById('cover1');
+let plate1 = "assets/pizza.svg";
+let plate2 = "assets/food.svg";
+let plate3 = "assets/chicken.svg";
 
 cover1.onclick = () => {
-    
+    cover1.src = plate1;
 }
